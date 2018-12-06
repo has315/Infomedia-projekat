a053916dadde82a25866e71dd24a46cacd84fea5
has315 <45577676+has315@users.noreply.github.com>
2018-12-06 01:03:13 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $hostname = "localhost";
 $username = "root";
-$password = "fnligvu5";
+$password = "";
 $database = "phpnewbiedb";
 $conn = mysqli_connect($hostname, $username, $password, $database);
 mysqli_select_db($conn, $database) or die( "Unable to select database");
